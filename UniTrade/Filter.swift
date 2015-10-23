5468eb1e1dceb8e8d9155e03f36fdff5fb00b3c4
EllisMin <dbmin@hotmail.com>
2015-10-23 16:59:15 -0500

    Added 4 Controllers.

---
+++ UniTrade/Filter.swift
@@ -0,0 +1,13 @@
+//
+//  Filter.swift
+//  UniTrade
+//
+//  Created by Ellis on 10/23/15.
+//  Copyright (c) 2015 Ellis. All rights reserved.
+//
+
+import UIKit
+
+class Filter: UIViewController {
+
+}
---
+++ User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  UniTrade
+//
+//  Created by Ellis on 10/23/15.
+//  Copyright (c) 2015 Ellis. All rights reserved.
+//
+
+import Cocoa
+
+class User: UIViewController {
+
+}
