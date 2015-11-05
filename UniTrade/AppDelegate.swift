5608779f8c4c48e3a60dc0606fe8e8006b63aaa4
EllisMin <dbmin@hotmail.com>
2015-11-04 22:08:45 -0600

    git commit -m "a"

--- UniTrade/AppDelegate.swift
+++ UniTrade/AppDelegate.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+//import Parse
+//import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
     var window: UIWindow?
 
+//    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+//        // [Optional] Power your app with Local Datastore. For more info, go to
+//        // https://parse.com/docs/ios_guide#localdatastore/iOS
+//        Parse.enableLocalDatastore()
+//        
+//        // Initialize Parse.
+//        Parse.setApplicationId("oEbnW1gX40JK1AJszs7lTQQzwGfqHDV1Q3PC3O6T",
+//            clientKey: "w5EMzQrAzQDWfmND3kGKryq7x3OxoBjDG6cO79nA")
+//        
+//        // [Optional] Track statistics around application opens.
+//        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+//        
+//        // ...
+//    }
+//    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
